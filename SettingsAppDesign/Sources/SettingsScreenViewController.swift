69f38c6d2a7d049bb7343815fd3155c64c18027c
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-02-09 18:22:03 +0900

    Initial Commit

---
+++ SettingsAppDesign/Sources/SettingsScreenViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class SettingsScreenViewController: UIViewController {
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setups
+
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+
+    private func setupHierarchy() { }
+
+    private func setupLayout() { }
+}
+

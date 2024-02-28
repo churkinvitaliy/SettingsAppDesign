012efc125f03641ff1ec8537e433598d32495d38
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-02-29 00:59:02 +0900

    Implemented a custom cell with a switch

--- SettingsAppDesign/Sources/SwitchTableViewCell.swift
+++ SettingsAppDesign/Sources/SwitchTableViewCell.swift
@@ -1,8 +1,43 @@
-//
-//  SwitchTableViewCell.swift
-//  SettingsAppDesign
-//
-//  Created by Churkin Vitaly on 27.02.2024.
-//
+import UIKit
+import SnapKit
 
-import Foundation
+class SwitchTableViewCell: SettingsTableViewCell {
+
+    // MARK: - Properties
+
+    override class var сellIdentifier: String { "switchCellIdentifier" }
+
+    // MARK: - Outlets
+
+    private lazy var switchControl: UISwitch = {
+        let switchControl = UISwitch()
+        return switchControl
+    }()
+
+    // MARK: - Initializers
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setups
+
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        contentView.addSubview(switchControl)
+    }
+
+    override func setupLayout() {
+        super.setupLayout()
+        switchControl.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalTo(contentView).inset(20)
+        }
+    }
+}

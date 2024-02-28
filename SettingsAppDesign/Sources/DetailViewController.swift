8b48a0d2cc549134c3b80d7e23107b6dd28d4e6b
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-02-29 01:01:26 +0900

    Blank screen with details

--- SettingsAppDesign/Sources/DetailViewController.swift
+++ SettingsAppDesign/Sources/DetailViewController.swift
@@ -1,29 +1,22 @@
-//
-//  DetailViewController.swift
-//  SettingsAppDesign
-//
-//  Created by Churkin Vitaly on 27.02.2024.
-//
-
 import UIKit
 
 class DetailViewController: UIViewController {
 
+    // MARK: - Properties
+
+    var setting: Setting?
+
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Setups
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupView() {
+        view.backgroundColor = .white
+        title = setting?.nameSetting
     }
-    */
-
 }
